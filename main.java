5b7d830cc8ecf6a5fb76881bded9604c4d8b9800
zayedalfalasi <zayed.alfalasi@marquette.edu>
2014-03-02 15:59:43 -0600

    Game Skeleton

---
+++ main.java
@@ -0,0 +1,73 @@
+import java.applet.*;
+import java.awt.*;
+
+/**
+ * Animation example using objects to create
+ * complicated patterns.
+ *
+ * @author mike slattery
+ * @version Jan 2014
+ */
+
+public class main extends Applet implements Runnable {
+
+  Thread anim = null;  // animation thread
+  
+  public static final int WIDTH = 700; // Applet size
+  public static final int HEIGHT = 500;
+  
+
+  
+  Image offscreen = null;
+  Graphics offgr;
+  
+  public void init() {
+
+  }
+
+  public void start() {	
+  	anim = new Thread(this);
+  	anim.start();
+  } 
+  
+  public void update(Graphics g)
+  {
+    // If the buffer doesn't exist, set it up
+  	if (offscreen == null)
+  	{
+  		offscreen = createImage(WIDTH,HEIGHT);
+  		offgr = offscreen.getGraphics();
+  	}
+  	// Call paint() to draw on the offscreen buffer
+  	paint(offgr);
+  	// And then copy that image to the screen
+  	g.drawImage(offscreen,0,0,null);
+  }
+ 
+  public void paint(Graphics g) {
+  
+    // Draw a black background
+    g.setColor(Color.black);
+    g.fillRect(0,0,WIDTH,HEIGHT);
+    
+
+  }
+  
+  public void run() {
+    while (anim != null)
+    {
+      // update positions for next frame
+
+      
+      repaint();
+      // wait a bit (frames per second about 33)
+      try {
+      	Thread.sleep(30);
+      } catch (InterruptedException e) {}
+    }
+  }
+  
+  public void stop() {  
+    anim = null;  // stop animation thread
+  } 
+}
