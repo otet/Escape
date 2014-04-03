33da098f0af7c31a65846455ae731df4c96a5615
oteplau <otegonzalez@gmail.com>
2014-04-03 15:53:03 -0500

    added intro screen
    we still need to write the string for how to play the game or if you
    want to make the intro screen a diff way then just let me know and
    change it. This is just an idea. text me after you check this out so i
    know what you think

--- Shoot.java
+++ Shoot.java
@@ -2,16 +2,23 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 import javax.swing.JApplet;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 /*	
- *  *Use W, S and Space for Player 1*
+ * 
  *  
- *  * Use Up Arrow, Down Arrow, and Enter for Player 2 *
+ *  * Use Left and Right arrow keys to move Player *
+ *  
  *  
  *  Created by José González & Zayed Al Falasi
  *  Prof. Michael Slattery
@@ -20,7 +27,7 @@ import javax.swing.JApplet;
  *  Spring 2014 
  */
 
-public class Shoot extends JApplet implements  Runnable {
+public class Shoot extends JApplet implements  Runnable{
 	
 	
 	int width = 400;
@@ -28,6 +35,11 @@ public class Shoot extends JApplet implements  Runnable {
 	
 	private Player player1;
 	public Block block1;
+	public Block block2;
+	public Block block3;
+	public Block block4;
+	public Block block5;
+	public Block block6;
 	public Block badBlock;
 	private Graphics graphics;
 	public Image image = null;
@@ -36,8 +48,14 @@ public class Shoot extends JApplet implements  Runnable {
 	public boolean player1Right = false;
 
 	CopyOnWriteArrayList<Bullet> bullets = new CopyOnWriteArrayList<Bullet>();
-	CopyOnWriteArrayList<Block> blocks = new CopyOnWriteArrayList<Block>();
-	Thread anim;
+//	CopyOnWriteArrayList<Block> blocks = new CopyOnWriteArrayList<Block>();
+	
+
+	private JButton start = new JButton ("Play Game");
+    String intro = "Waddup";
+
+	
+	Thread anim = null;
 	
 	
 	
@@ -52,10 +70,50 @@ public class Shoot extends JApplet implements  Runnable {
 		
 		player1 = new Player(200, 427, 20, 90, 10, Color.BLACK);
 		
-		block1 = new Block(block1, 200, 100, 20, 20, Color.BLACK, false);
+		block1 = new Block(block1, 200, 100, 2, 20, 20, Color.BLACK, false);
+		block2 = new Block(block2, 100, 25, 3, 20, 20, Color.BLACK, false);
+		block3 = new Block(block3, 370, 60, 2, 20, 20, Color.BLACK, false);
+		block4 = new Block(block4, 270, 8, 1, 20, 20, Color.BLACK, false);
+		block5 = new Block(block5, 145, 4, 2, 20, 20, Color.BLACK, false);
+		block6 = new Block(block6, 40, 42, 3, 20, 20, Color.BLACK, false);
+
 		
-		Block evilBlock = new Block(badBlock, 250, 85, 20, 20, Color.BLACK, false);
-		blocks.add(evilBlock);
+	/*	Block evilBlock = new Block(badBlock, 250, 85, 20, 20, Color.BLACK, false);
+		blocks.add(evilBlock);*/
+		
+		try {
+            SwingUtilities.invokeAndWait(new Runnable() {
+                public void run() {
+                    createGUI();
+                   
+                }
+            });
+        } catch (Exception r) { 
+            //System.err.println("method didn't complete successfully");
+        }
+		
+		 start.addActionListener(new ActionListener() {
+	            public void actionPerformed(ActionEvent e) {
+	            
+
+
+	         
+	          
+	            //	repaint();
+	            	
+	        	String title = "Escape!";  
+	            String message = "YEAH FUN!\nWatch out for the bad guys!";
+	            JOptionPane.showMessageDialog(null, message, title, JOptionPane.INFORMATION_MESSAGE);
+	            
+	            anim.start();
+	            
+	           
+	            
+	          
+
+	            System.out.println("Button was clicked");
+	            }
+	          });
 		
 		
 		System.out.println("init() done");
@@ -71,7 +129,7 @@ public class Shoot extends JApplet implements  Runnable {
 	public void start(){
 		
 		anim = new Thread(this);
-		anim.start();
+	//	anim.start();
 		
 		System.out.println("start() done");
 	}
@@ -98,10 +156,10 @@ public class Shoot extends JApplet implements  Runnable {
 		for (Bullet bullet : bullets){
 			bullet.draw(g);
 			}
-		for (Block evilBlock : blocks){
+		/*for (Block evilBlock : blocks){
 			evilBlock.draw(g);
 		//	evilBlock.update(this, 0);
-			}
+			}*/
 		} else if (player1.getHealth() == 0){
 			g.setColor(Color.yellow);
 			g.drawString("You Lose!", 250, 190);
@@ -120,21 +178,106 @@ public class Shoot extends JApplet implements  Runnable {
 	    	
 	    }else{
 		    block1.draw(g);
-		   
+		 }
+			    
+		if(block2.isHit()){
+			
+			  }else{
+				  block2.draw(g);  
+			    }
+
+		if(block3.isHit()){
+			
+		}else{
+		    block3.draw(g);		   
+		}
+
+		if(block4.isHit()){
+			
+		}else{
+		    block4.draw(g);
+		}
+		
+		if(block5.isHit()){
+	    	
+	    }else{
+		    block5.draw(g);	   
+	    }
+		
+	    if(block6.isHit()){
+	    	
+	    }else{
+		    block6.draw(g);		   
 	    }
 		
 		
 	}
 	
+    private void createGUI() {
+        JPanel panel = new JPanel();
+        
+        panel.setBackground(Color.gray);
+        panel.setSize(600,500);
+        panel.add(start);
+        panel.setOpaque(true); 
+        setContentPane(panel);
+        this.add(panel);
+        
+      
+        
+    }   
+	
 	public void gameUpdate(){
 		player1.update(this, 1);	
 		block1.update(this, 2);
+		block2.update(this, 2);
+		block3.update(this, 2);
+		block4.update(this, 2);
+		block5.update(this, 2);
+		block6.update(this, 2);
 		
 		for (Bullet bullet : bullets){
 			bullet.update(this, 0);
 			}
 		
 	}
+	
+	/*public void setLevel1()
+	{
+		bullets = new Bullet[5];
+		bullets[1] = new Bullet(player1, -8, player1.getxPos() + 9, player1.getyPos(), 2, 10, Color.BLACK);
+	}
+	*/
+	
+/*	public void collision (){
+		ListIterator<Block> blockIterator = blocks.listIterator();
+	   
+		for(Bullet b : bullets){
+	        Rectangle rB = b.getBoundingBox();
+	      
+
+	        for(Block t : blocks){
+	            Rectangle rE = t.getBoundingBox();
+	            while (blockIterator.hasNext()) {
+				    Rectangle bb = blockIterator.next().getBoundingBox();
+				    if(rB.intersects(bb)) {
+				        blockIterator.remove();
+				    }
+				}
+	          
+	            if(rB.intersects(rE)){
+	               blocks.remove(t);
+	               System.out.println("Got Hit");
+	            }
+	        }
+	    
+	
+		
+	
+	    }
+	    
+	}*/
+	
 	
 	class KeyL extends KeyAdapter {
 		
@@ -185,8 +328,9 @@ public class Shoot extends JApplet implements  Runnable {
 		
 		
 	}
+		}
 
-}
+
 	
 	
 	
