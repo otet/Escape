6bfc47bf80ce3cf8093051e9386090415ecb7916
oteplau <otegonzalez@gmail.com>
2014-04-03 16:19:53 -0500

    something didnt sync right
    read the last msg. now it works

--- Block.java
+++ Block.java
@@ -12,6 +12,7 @@ public class Block extends GameInit {
 	private Color c;
 	private Block block;
 	private boolean hit;
+	private int dy;
 	
 	
 	
@@ -19,10 +20,11 @@ public class Block extends GameInit {
 	
 
 
-	public Block(final Block block, final int xPos, final int yPos, final int width, final int height, final Color c, final boolean hit ){
+	public Block(final Block block, final int xPos, final int yPos, final int dy, final int width, final int height, final Color c, final boolean hit ){
 		this.setBlock(block);
 		this.xPos = xPos;
 		this.yPos = yPos;
+		this.dy = dy;
 		this.height = height;
 		this.width = width;
 		this.rect = new Rectangle(xPos, yPos, 20, 20);
@@ -53,13 +55,46 @@ public class Block extends GameInit {
 		return Toolkit.getDefaultToolkit().getImage(img);
 	}
 
-
+	
 	@Override
 	void update(final Shoot shooting, final int id) {
 		
-		
-	
-			
+			if (shooting.block1.getyPos()== 490){
+				shooting.block1.setyPos(0);
+			}
+			if (shooting.block2.getyPos()== 490){
+				shooting.block2.setyPos(0);
+			}
+			if (shooting.block3.getyPos()== 490){
+				shooting.block3.setyPos(0);
+			}
+			if (shooting.block4.getyPos()== 490){
+				shooting.block4.setyPos(0);
+			}
+			if (shooting.block5.getyPos()== 490){
+				shooting.block5.setyPos(0);
+			}
+			if (shooting.block6.getyPos()== 490){
+				shooting.block6.setyPos(0);
+			}
+		if(!shooting.block1.isHit()){
+				yPos += dy;
+			} 
+			else if(!shooting.block2.isHit()){
+				yPos += dy;
+			}
+			else if(!shooting.block3.isHit()){
+				yPos += dy;
+			}
+			else if(!shooting.block4.isHit()){
+				yPos += dy;
+			}
+			else if(!shooting.block5.isHit()){
+				yPos += dy;
+			}
+			else if(!shooting.block6.isHit()){
+				yPos += dy;
+			}
 			
 			
 			}
@@ -88,6 +123,16 @@ public class Block extends GameInit {
 
 	public void setHit(boolean hit) {
 		this.hit = hit;
+	}
+
+
+	public int getDy() {
+		return dy;
+	}
+
+
+	public void setDy(int dy) {
+		this.dy = dy;
 	}
 		
 	}
